be4d54356bb06b894baccf6d26021ab9202f2b8b
Kyle Lemmon <kylej.lemmon@gmail.com>
2021-01-25 07:06:17 +0000

    create isOdd function exercise

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(num){
+
+  return num % 2 !== 0
+
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("10 is odd: " + isOdd(10));
+console.log("15 is odd: " + isOdd(15));
